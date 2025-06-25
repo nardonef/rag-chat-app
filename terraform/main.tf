9c933a763f7214dda545c9424bf7b4574769ade7
Frank Nardone <fnardone@Franks-MacBook-Pro-2.local>
2025-06-25 09:13:03 -0400

    try string

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "google" {
 
 resource "google_artifact_registry_repository" "app_repo" {
   name     = var.repo_name
-  repository_id = var.repo_id
+  repository_id = "rag-chat-app"
   format   = "DOCKER"
   location = var.region
 }
